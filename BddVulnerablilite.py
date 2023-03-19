31607277b5822aeeba14821ad9cf6037beb23878
ewan quioc <quioc.ewan@gmail.com>
2023-03-19 15:27:30 +0100

    ajout de la partie symfony et insertion de la cve en bdd avec héritage et check si elle existe déjà

---
+++ BddVulnerablilite.py
@@ -0,0 +1,50 @@
+import mysql.connector
+class BddVulnerablilite:
+    def __init__(self, host, user, password, database):
+        self.host = host
+        self.user = user
+        self.password = password
+        self.database = database
+        self.mydb = ""
+
+    # initialisation de la connexion
+    def CreateConnexion(self):
+        try:
+            self.mydb = mysql.connector.connect(
+                host=self.host,
+                user=self.user,
+                password=self.password,
+                database=self.database,
+            )
+            return True
+        except:
+            return False
+
+
+    def checkCVE(self, cve):
+        cursor = self.mydb.cursor()
+        req = ("SELECT id FROM vulnerability WHERE cve = %s")
+        cursor.execute(req, (cve,))
+        if cursor.fetchone() == None:
+            return True
+        else:
+            return False
+
+    def insertCVE(self, cve, link, vulnType):
+        if self.checkCVE(cve):
+            cursor = self.mydb.cursor()
+            req = ("INSERT INTO vulnerability(cve, link, discr) VALUES(%s,%s,%s)")
+            data = (cve, link, vulnType)
+            cursor.execute(req, data)
+            self.mydb.commit()
+            req = ("SELECT id FROM vulnerability WHERE cve = %s")
+            cursor.execute(req, (cve,))
+            idVuln = cursor.fetchone()
+            req = ("INSERT INTO "+vulnType+"(id) VALUES(%s)")
+            data = (idVuln)
+            cursor.execute(req, data)
+            self.mydb.commit()
+            cursor.close()
+            return "La CVE : " + cve + " à bien été ajoutée"
+        else :
+            return "La CVE : " + cve + " est déjà enrgistrée"
--- main.py
+++ main.py
@@ -1,23 +1,35 @@
 import requests
 from bs4 import BeautifulSoup
+from BddVulnerablilite import BddVulnerablilite
+import json
 
-def getCVE(url, soup, alertType):
+def getCVE(connexionBdd, url, soup, alertType):
     for element in soup.find_all('div', attrs={"class": alertType}):
         for link in element.find_next("a"):
             match alertType:
                 case "cert-alert":
-                    print(url+"alerte/"+link.text)
-                    pass
+                    print(connexionDb.insertCVE(link.text, url+"alerte/"+link.text, "alerte"))
                 case "cert-avis":
-                    print(url+"avis/"+link.text)
+                    print(connexionDb.insertCVE(link.text, url+"avis/"+link.text, "avis"))
 
 url = "https://www.cert.ssi.gouv.fr/"
 
 if __name__ == '__main__':
-    page = requests.get(url).text
-    soup = BeautifulSoup(page, "html.parser")
-    # récupération des alertes du cert
-    getCVE(url, soup, "cert-alert")
-    # récupération des avis du cert
-    getCVE(url, soup, "cert-avis")
+    with open("config.json") as file:
+        data = json.load(file)
+        host = data["host"]
+        username = data["username"]
+        password = data["password"]
+        database = data["database"]
+        connexionDb = BddVulnerablilite(host, username, password, database)
+        if connexionDb.CreateConnexion():
+            checkConnexion = True
+        else:
+            checkConnexion = False
+    if checkConnexion:
+        page = requests.get(url).text
+        soup = BeautifulSoup(page, "html.parser")
+        # récupération des alertes du cert
+        getCVE(connexionDb, url, soup, "cert-alert")
+        getCVE(connexionDb, url, soup, "cert-avis")
 
